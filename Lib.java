3c214e45aecfeaca4f6c57d0a612b7488004b1eb
Jose <jose_campillos_1997@hotmail.com>
2017-03-15 17:44:33 +0100

    modificaciones alumno 1

--- Lib.java
+++ Lib.java
@@ -7,6 +7,7 @@ public class Lib {
 
     public void metodoAlumno1(){
         metodoAlumno2();
+	System.out.println("modificado por alumno 1"); // alumno 1
     }
 
     public void metodoAlumno2(){
@@ -18,6 +19,8 @@ public class Lib {
         System.out.println("cambio en master");
 
         System.out.println("cambio en metodo3");
+	// modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
 
     }
 	public void metodo3(){
